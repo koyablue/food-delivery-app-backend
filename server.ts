947b8a85310b8161c811ded2e47e7b2064967ef4
koyaaoyama <aoyama.ko0920@gmail.com>
2023-08-30 21:27:55 -0700

    Environment setup and build docker container

---
+++ server.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import container from './core/container.core';
+
+export const server = new InversifyExpressServer(container);
+server.setConfig(app => {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+});
