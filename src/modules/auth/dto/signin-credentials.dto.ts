92c898613898aaa1d994f6ca628fef8f8ce0a1aa
koyaaoyama <aoyama.ko0920@gmail.com>
2023-09-02 15:21:52 -0700

    Implement AuthRepository. Implement generic error handlings

---
+++ src/modules/auth/dto/signin-credentials.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator"
+export class SignInCredentialsDto {
+    @IsEmail()
+    email: string
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(10)
+    password: string
+}
